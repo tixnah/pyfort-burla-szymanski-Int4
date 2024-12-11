ad21a0035723fae7e7b24af1890b7e506e6475b0
cristina <cristinaburlaa@gmail.com>
2024-12-11 20:59:59 +0100

    first tries of chance challenges

--- chance_challenges.py
+++ chance_challenges.py
@@ -1,2 +1,20 @@
-def shell_game(to_guess, user_guess):
+import random
+def shell_game():
+    L = ['A', 'B', 'C']
+    attempt = 1
+    key_count = 0
 
+    while attempt <= 2 :
+        guess = random.choice(L)
+        user_guess = input("Choose a shell between A, B or C : ")
+
+        if user_guess in L :
+            if user_guess == guess :
+                print("Correct ! You win a key !")
+                key_count += 1
+            else :
+                print("Wrong choice, you got", 2 - attempt, "attempt left.")
+        else :
+            print("Wrong input.")        #Verifies if the user's input is really one of the options
+
+        attempt += 1
--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-import random
 from chance_challenges import *
 
 # implementation of the shell game
 print("Welcome to the shell game !")
 print("You must guess which of the three shells (A, B, or C) hides the key. You have two attempts to find it. On each attempt, the key is randomly placed under one of the shells.")
 print("Good luck ;)")
-L = ['A', 'B', 'C']
-key_count = 0
-guess = False
-attempt = 1
-for attempt in range(2):
-    to_guess = random.choice(L)
-    user_guess = input("Choose a shell between A, B and C : ")
-    attempt += 1
-    if shell_game(to_guess, user_guess):
-        print("Correct ! You win a key !")
-        key_count += 1
-    else:
-        print("Wrong choice, you got", 2 - attempt, "attempts left :(")
+shell_game()
+
 
 # implementation of the rolling dice game
