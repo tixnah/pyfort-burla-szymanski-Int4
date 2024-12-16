8f78026cc0c49da0e54f7e5778c83e9d3a710d17
cristina <cristinaburlaa@gmail.com>
2024-12-16 15:23:01 +0100

    the final version of the first chance challenge
    Signed-off-by: cristina <cristinaburlaa@gmail.com>

--- chance_challenges.py
+++ chance_challenges.py
@@ -3,8 +3,9 @@ def shell_game():
     L = ['A', 'B', 'C']
     attempt = 1
     key_count = 0
+    found = False
 
-    for i in range(2) :
+    while found == False and attempt <= 2 :
         guess = random.choice(L)
         user_guess = input("Choose a shell between A, B or C : ")
 
@@ -15,9 +16,12 @@ def shell_game():
             if user_guess == guess :
                 print("Correct ! You win a key !")
                 key_count += 1
+                found = True
             elif user_guess != guess and attempt < 2 :
                 print("Wrong choice, you got", 2 - attempt, "attempt left.")
+                found = False
             elif user_guess != guess and attempt == 2:
                 print("You lose...")
+                found = False
         attempt += 1
 
