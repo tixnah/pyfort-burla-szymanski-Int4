8396f260d3d98e32d2983ea9b79c5adfc79203f8
laure <laure.szymanski@efrei.net>
2024-12-20 08:23:04 +0100

    mathematical challenged ( doesnt work the best i need to do some changes) and main with count of keys.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from chance_challenges import *
+#init of key_count
+key_count=0
+print("You have {} keys".format(key_count))
 
-# implementation of the shell game
-print("Welcome to the shell game !")
-print("You must guess which of the three shells (A, B, or C) hides the key. You have two attempts to find it. On each attempt, the key is randomly placed under one of the shells.")
-print("Good luck ;)")
-shell_game()
+from mathematique_chal import *
 
+if math_challenge_prime():
+    print("Correct! You win a key.")
+    key_count+=1
+else:
+    print("do better next time, you've just lost an opportunity to win a key!")
+print("You have {} keys".format(key_count))
 
-# implementation of the rolling dice game
-print("Welcome to the rolling dice game !")
-print("You play against the Game Master. The first to roll a 6 wins ! You have 3 attempts to try to win.")
-print("Good luck ;)")
-roll_dice_game()
+from mathematique_chal import *
+if math_roulette_challenge():
+    print("Correct! You win a key.")
+    key_count+=1
+else:
+    print("You lost the opportunity to win a key!")
+
+print("You have {} keys".format(key_count))
+
---
+++ mathematique_chal.py
@@ -0,0 +1,68 @@
+#prime numbers challenge
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2,n):
+        if n%i==0 and i!=n:
+            return False
+        else:
+            return True
+
+def nearest_prime(n):
+    prime_list = []
+    for i in range(n+1,2*n):
+        if is_prime(i):
+            prime_list.append(i)        # we are adding al the prime number from 2 to 2n and adding it to a list.
+    return prime_list[0]
+
+    # we are looking for the value with the minimum difference from n, and attributing it to a variable.
+
+
+import random
+def math_challenge_prime():
+    n=random.randint(10,20)
+    players_answer=int(input("find the prime number closer to {}".format(n)))
+    print(nearest_prime(n))
+    if nearest_prime(n)==players_answer:
+        return True
+    else:
+        return False
+
+#math Roulette challenge
+def math_roulette_challenge():
+    random_list=[]
+    for i in range (5):
+        random_list.append(random.randint(1,20))
+    print("Numbers on the roulette are:")
+    print(random_list)
+    operator=random.choice(['addition','subtraction','multiplication'])
+    print("Calculate the result by combining these numbers with {}".format(operator))
+    players_answer=int(input("Your answer:"))
+    if operator=='addition':
+        sum(random_list)
+    if operator=='subtraction':
+        val = random_list[0]
+        for num in random_list[1:]:
+            val -= num
+    if operator=='multiplication':
+        val=1
+        for num in random_list:
+            val*=num
+
+    if players_answer==val:
+        return True
+    else:
+        return False
+
+#linear equation
+ def solve_linear_equation():
+     a=random.randint(1,10)
+     b=random.randint(1,10)
+     return -b/a
+ def math_challenge_equation():
+
+
+
+
+
